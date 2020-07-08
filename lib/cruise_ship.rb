5edb4de3a7149010e061a9fc3ebab640698ec589
Jzavier Timm <jztimm@gmail.com>
2020-07-08 20:21:19 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,7 +8,7 @@
 
 def select_winner(passengers)
   winner = ""
-  passengers.each do |suite, names|
+  passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name
     end
